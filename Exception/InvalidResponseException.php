216d684a767a53b8631443c6ddc701045e1b8875
Api Postcode <info@api-postcode.nl>
2017-06-04 09:46:02 +0200

    Create InvalidResponseException.php

---
+++ Exception/InvalidResponseException.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* (c) Api Postcode <info@api-postcode.nl>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace ApiPostcode\Exception;
+
+/**
+ * Class InvalidResponseException
+ *
+ * @author Api Postcode <info@api-postcode.nl>
+ */
+class InvalidResponseException extends \Exception
+{
+
+}
